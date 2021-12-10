95a079a7b60c4a257c5d68e041c3192a8e2ebe50
Camilo Diaz <k.diaz99@gmail.com>
2021-12-10 16:02:33 -0500

    drawing 3 mosquitos in random positions accross the screen

--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ import cv2
 from skimage import io
 from pathlib import Path
 import numpy as np
-import game_object
+from game_object import game_object
 import overlay_drawer
 
 
@@ -10,42 +10,31 @@ video_cap = cv2.VideoCapture(0)
 video_cap.set(cv2.CAP_PROP_FRAME_WIDTH ,1280)
 video_cap.set(cv2.CAP_PROP_FRAME_HEIGHT ,720)
 
-controller_up = cv2.imread("../imgs/up1.png")
-controller_down = cv2.imread("../imgs/down.png")
-controller_right = cv2.imread("../imgs/right.png")
-controller_left = cv2.imread("../imgs/left.png")
-controller_button = cv2.imread("../imgs/button.png")
 
-up_height,up_width,channels = controller_up.shape
-down_height,down_width,channels = controller_down.shape
-right_height,right_width,channels = controller_right.shape
-left_height,left_width,channels = controller_left.shape
-button_height,button_width,channels = controller_button.shape
-
-print(controller_right.shape)
 
 #list of game objects
 scene_objects = []
 
+
+
+for i in range(3):
+    pos_x = np.random.randint(640)
+    pos_y = np.random.randint(360)
+    pos = np.array([pos_x,pos_y])
+    mosquito = game_object("../imgs/mosquito2.png",pos)
+    scene_objects.append(mosquito)
+
+
 alpha = 0.01
 #PRESS 'ESC' TO CLOSE THE WINDOW 
 while cv2.waitKey(1) !=27:
-    success, img = video_cap.read()
-    img = cv2.flip(img,1)
-    img_width,img_height,img_channels = img.shape
-    added_image_up,up_posx_1,up_posy_1,up_posx_2,up_posy_2 = overlay_drawer.draw_up_pad(img,alpha,controller_up,img_height,img_width,up_width,up_height)
-    img[up_posx_1:up_posx_2,up_posy_1:up_posy_2,:] = added_image_up
-    added_image_down,up_posx_1,up_posy_1,up_posx_2,up_posy_2 = overlay_drawer.draw_down_pad(img,alpha,controller_down,img_height,img_width,down_width,down_height)
-    img[up_posx_1:up_posx_2,up_posy_1:up_posy_2,:] = added_image_down
-    added_image_right,up_posx_1,up_posy_1,up_posx_2,up_posy_2 = overlay_drawer.draw_right_pad(img,alpha,controller_right,img_height,img_width,right_width,right_height)
-    img[up_posx_1:up_posx_2,up_posy_1:up_posy_2,:] = added_image_right
-    added_image_left,up_posx_1,up_posy_1,up_posx_2,up_posy_2 = overlay_drawer.draw_left_pad(img,alpha,controller_left,img_height,img_width,left_width,left_height)
-    img[up_posx_1:up_posx_2,up_posy_1:up_posy_2,:] = added_image_left
-    added_image_button,up_posx_1,up_posy_1,up_posx_2,up_posy_2 = overlay_drawer.draw_button(img,alpha,controller_button,img_height,img_width,button_width,button_height,0)
-    img[up_posx_1:up_posx_2,up_posy_1:up_posy_2,:] = added_image_button
-    added_image_button,up_posx_1,up_posy_1,up_posx_2,up_posy_2 = overlay_drawer.draw_button(img,alpha,controller_button,img_height,img_width,button_width,button_height,button_width +20)
-    img[up_posx_1:up_posx_2,up_posy_1:up_posy_2,:] = added_image_button
+    success, screen_buffer = video_cap.read()
+    screen_buffer = cv2.flip(screen_buffer,1)
+    img_width,img_height,img_channels = screen_buffer.shape
+    for i in range(len(scene_objects)):
+        scene_objects[i].draw(screen_buffer,alpha)
+    
 
-    cv2.imshow("Image",img)
+    cv2.imshow("Image",screen_buffer)
 
 
